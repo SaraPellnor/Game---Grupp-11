f0bc0ceb1aea92c938dd84b0e1be56931f62b22f
SigneAnnaMaria <signe.annamaria@gmail.com>
2022-10-11 14:55:01 +0200

    localstorage start

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 
 
 const circles = document.querySelectorAll(".circle");
+const btnBlue = document.querySelector(".btnBlue");
+const btnRed = document.querySelector(".btnRed");
+
+btnBlue.addEventListener("click", blue);
+btnRed.addEventListener("click", red);
+
+const goBlue = goBlue;
+
+function blue() {
+    localStorage.setItem("player", goBlue);
+}
+
 
 for (const circle of circles) {
     circle.addEventListener("click", playerTwo);
 }
 
-
 function playerTwo(e) {
     e.target.style.backgroundColor = "red"
-
 }
+
